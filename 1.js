1372a98853b2d39840dc1531860210bc2655eedd
HUkhan92 <hammadacg@gmail.com>
2024-07-01 15:11:08 +0500

    first commit

---
+++ 1.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let ans = await inquirer.prompt([{
+        name: "a1",
+        type: "number",
+        message: "put first number of sequence"
+    }]);
+let answ = await inquirer.prompt([{
+        name: "a2",
+        type: "number",
+        message: "put second number of sequence"
+    }]);
+let find = await inquirer.prompt([{
+        name: "n",
+        type: "number",
+        message: "put the no of nth term "
+    }]);
+let c = (find.n - 1);
+let d = answ.a2 - ans.a1;
+console.log(ans.a1 + c * d);
---
+++ README.md
@@ -0,0 +1,1 @@
+"# sequence_of_series" 
